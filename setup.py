d4a1899b96091765d0dbb33ab24a23940843cb6f
Heikki Juva <juva.hj@gmail.com>
2022-09-07 10:51:50 +0300

    Refactored and added setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,17 @@ from setuptools import setup
 setup(
     name='logz_reader',
     version='0.1',
-    packages=[''],
-    url='',
-    license='MIT',
-    author='Heikki "Zokol" Juva',
-    author_email='heikki@juva.lu',
+    packages=['logzreader'],
+    install_requires=[
+        'click>=8.1.3',
+        'requests>=2.28.1',
+        'pytz>=2022.2.1',
+        'python-dotenv>=0.21.0'
+    ],
+    tests_require=['unittest'],
+    url='https://github.com/Zokol/logz_reader',
+    license='Creative Commons Attribution-ShareAlike 4.0 International',
+    author='Heikki "zokol" Juva',
     description='Logz.io API Reader'
 )
+
